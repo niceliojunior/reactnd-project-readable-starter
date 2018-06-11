914876640c484a2ab7c389b836000594d28d4067
System Administrator <root@Junecas-iMac.local>
2018-06-11 00:52:21 -0300

    add category view

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import NavBar from './NavBar';
 import Home from './Home';
+import Category from './Category';
 
 
 export default class App extends React.Component {
@@ -12,6 +13,7 @@ export default class App extends React.Component {
         <div>
           <NavBar />
           <Route exact path='/' component={Home} />
+          <Route exact path='/category/:id' component={Category} />
         </div>
       </Router>
     );
